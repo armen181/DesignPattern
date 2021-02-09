973d6983c676f54325723713a442b51ad38bc74f
Armen <armen.181@gmail.com>
2021-02-09 20:40:38 +0400

    single responsibility

---
+++ src/am/armen181/single_responsibility/Finance.java
@@ -0,0 +1,8 @@
+package am.armen181.single_responsibility;
+
+public class Finance {
+
+    public double calcIncomeTaxForCurrentYear(final Employee employee) {
+        return 0;
+    }
+}
---
+++ src/am/armen181/single_responsibility/HrPromotions.java
@@ -0,0 +1,8 @@
+package am.armen181.single_responsibility;
+
+public class HrPromotions {
+
+    public boolean isPromotionDueThisYear(final Employee employee) {
+        return false;
+    }
+}
